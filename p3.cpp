4b5235136bc5be9839ca612032072655ec6aa036
SameerSingh369 <samy@s.gov>
2024-12-16 01:07:21 +0530

    update

--- p3.cpp
+++ p3.cpp
@@ -1,18 +1,18 @@
 // Longest Substring Without Repeating Character Problem Complete Solution
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int>CHI;
-        int maxLength = 0;
-        int start = 0;
-        for(int i = 0; i< s.length(); i++){
-            char ai = s[i];
-            if(CHI.find(ai) != CHI.end() && CHI[ai] >= start){
-            start = CHI[ai]+1;
-            }
-            CHI[ai]=i;
-            maxLength = max(maxLength, i-start+1);
-        }
-        return maxLength;
-    }
-};
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         unordered_map<char, int>CHI;
+//         int maxLength = 0;
+//         int start = 0;
+//         for(int i = 0; i< s.length(); i++){
+//             char ai = s[i];
+//             if(CHI.find(ai) != CHI.end() && CHI[ai] >= start){
+//             start = CHI[ai]+1;
+//             }
+//             CHI[ai]=i;
+//             maxLength = max(maxLength, i-start+1);
+//         }
+//         return maxLength;
+//     }
+// };
