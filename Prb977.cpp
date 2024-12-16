eb4fb26fab16df43f43899e58261057c0fb9a7ac
SameerSingh369 <samy@s.gov>
2024-12-16 19:02:46 +0530

    update

---
+++ Prb977.cpp
@@ -0,0 +1,11 @@
+#Squares of a Sorted Array Problem Complete Solution
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] *= nums[i];
+        }
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
